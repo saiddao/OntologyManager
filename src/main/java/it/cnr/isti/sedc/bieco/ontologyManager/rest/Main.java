ed6380a17db68d5dc1704ab9fcb99f2b461fcc5e
saiddao <saiddao@gmail.com>
2023-01-19 12:11:10 +0100

    Changed Port

--- src/main/java/it/cnr/isti/sedc/bieco/ontologyManager/rest/Main.java
+++ src/main/java/it/cnr/isti/sedc/bieco/ontologyManager/rest/Main.java
@@ -13,8 +13,8 @@ import java.net.URI;
  */
 public class Main {
 	// Base URI the Grizzly HTTP server will listen on
-	// public static final String BASE_URI = "http://127.0.0.1:8181/";
-	public static final String BASE_URI = "http://localhost:8181/";
+	// public static final String BASE_URI = "http://127.0.0.1:8282/";
+	public static final String BASE_URI = "http://localhost:8282/";
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
